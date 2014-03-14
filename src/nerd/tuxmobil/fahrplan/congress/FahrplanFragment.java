johnjohndoe/CampFahrplan

commit 92c52aa49bb8c5719f863f2a68bd93d2207e7844

Avoid hard-coding database columns indices.

src/nerd/tuxmobil/fahrplan/congress/FahrplanFragment.java
@@ -659,27 +659,27 @@ public class FahrplanFragment extends SherlockFragment implements OnClickListene
 
 		cursor.moveToFirst();
 		while (!cursor.isAfterLast()) {
-			Lecture lecture = new Lecture(cursor.getString(0));
+			Lecture lecture = new Lecture(cursor.getString(cursor.getColumnIndex(LecturesTable.Columns.EVENT_ID)));
 
-			lecture.title = cursor.getString(1);
-			lecture.subtitle = cursor.getString(2);
-			lecture.day = cursor.getInt(3);
-			lecture.room = cursor.getString(4);
-			lecture.startTime = cursor.getInt(5);
-			lecture.duration = cursor.getInt(6);
-			lecture.speakers = cursor.getString(7);
-			lecture.track = cursor.getString(8);
-			lecture.type = cursor.getString(9);
-			lecture.lang = cursor.getString(10);
-			lecture.abstractt = cursor.getString(11);
-			lecture.description = cursor.getString(12);
-			lecture.relStartTime = cursor.getInt(13);
-			lecture.date = cursor.getString(14);
-			lecture.links = cursor.getString(15);
-			lecture.dateUTC = cursor.getLong(16);
-			lecture.room_index = cursor.getInt(17);
-			lecture.recordingLicense = cursor.getString(18);
-			lecture.recordingOptOut = cursor.getInt(19) == LecturesTable.Values.REC_OPTOUT_OFF ? Lecture.RECORDING_OPTOUT_OFF : Lecture.RECORDING_OPTOUT_ON;
+			lecture.title = cursor.getString(cursor.getColumnIndex(LecturesTable.Columns.TITLE));
+			lecture.subtitle = cursor.getString(cursor.getColumnIndex(LecturesTable.Columns.SUBTITLE));
+			lecture.day = cursor.getInt(cursor.getColumnIndex(LecturesTable.Columns.DAY));
+			lecture.room = cursor.getString(cursor.getColumnIndex(LecturesTable.Columns.ROOM));
+			lecture.startTime = cursor.getInt(cursor.getColumnIndex(LecturesTable.Columns.START));
+			lecture.duration = cursor.getInt(cursor.getColumnIndex(LecturesTable.Columns.DURATION));
+			lecture.speakers = cursor.getString(cursor.getColumnIndex(LecturesTable.Columns.SPEAKERS));
+			lecture.track = cursor.getString(cursor.getColumnIndex(LecturesTable.Columns.TRACK));
+			lecture.type = cursor.getString(cursor.getColumnIndex(LecturesTable.Columns.TYPE));
+			lecture.lang = cursor.getString(cursor.getColumnIndex(LecturesTable.Columns.LANG));
+			lecture.abstractt = cursor.getString(cursor.getColumnIndex(LecturesTable.Columns.ABSTRACT));
+			lecture.description = cursor.getString(cursor.getColumnIndex(LecturesTable.Columns.DESCR));
+			lecture.relStartTime = cursor.getInt(cursor.getColumnIndex(LecturesTable.Columns.REL_START));
+			lecture.date = cursor.getString(cursor.getColumnIndex(LecturesTable.Columns.DATE));
+			lecture.links = cursor.getString(cursor.getColumnIndex(LecturesTable.Columns.LINKS));
+			lecture.dateUTC = cursor.getLong(cursor.getColumnIndex(LecturesTable.Columns.DATE_UTC));
+			lecture.room_index = cursor.getInt(cursor.getColumnIndex(LecturesTable.Columns.ROOM_IDX));
+			lecture.recordingLicense = cursor.getString(cursor.getColumnIndex(LecturesTable.Columns.REC_LICENSE));
+			lecture.recordingOptOut = cursor.getInt(cursor.getColumnIndex(LecturesTable.Columns.REC_OPTOUT)) == LecturesTable.Values.REC_OPTOUT_OFF ? Lecture.RECORDING_OPTOUT_OFF : Lecture.RECORDING_OPTOUT_ON;
 
 			MyApp.lectureList.add(lecture);
 			cursor.moveToNext();
@@ -737,8 +737,8 @@ public class FahrplanFragment extends SherlockFragment implements OnClickListene
 
 		hCursor.moveToFirst();
 		while (!hCursor.isAfterLast()) {
-			String lecture_id = hCursor.getString(1);
-			int highlightState = hCursor.getInt(2);
+			String lecture_id = hCursor.getString(cursor.getColumnIndex(HighlightsTable.Columns.EVENT_ID));
+			int highlightState = hCursor.getInt(cursor.getColumnIndex(HighlightsTable.Columns.HIGHLIGHT));
 			MyApp.LogDebug(LOG_TAG, "lecture "+lecture_id+" is hightlighted:" + highlightState);
 
 			for (Lecture lecture : MyApp.lectureList) {
@@ -784,7 +784,7 @@ public class FahrplanFragment extends SherlockFragment implements OnClickListene
 
 		alarmCursor.moveToFirst();
 		while (!alarmCursor.isAfterLast()) {
-			String lecture_id = alarmCursor.getString(5);
+			String lecture_id = alarmCursor.getString(alarmCursor.getColumnIndex(AlarmsTable.Columns.EVENT_ID));
 			MyApp.LogDebug(LOG_TAG, "lecture "+lecture_id+" has alarm");
 
 			for (Lecture lecture : MyApp.lectureList) {

src/nerd/tuxmobil/fahrplan/congress/AlarmList.java
@@ -1,6 +1,7 @@
 package nerd.tuxmobil.fahrplan.congress;
 
 import nerd.tuxmobil.fahrplan.congress.FahrplanContract.AlarmsTable;
+import nerd.tuxmobil.fahrplan.congress.FahrplanContract.LecturesTable;
 import nerd.tuxmobil.fahrplan.congress.FahrplanContract.AlarmsTable.Columns;
 
 import com.actionbarsherlock.app.SherlockListActivity;
@@ -97,13 +98,13 @@ public class AlarmList extends SherlockListActivity {
 
 		Intent intent = new Intent(this, AlarmReceiver.class);
 
-		String lecture_id = cursor.getString(5);
+		String lecture_id = cursor.getString(cursor.getColumnIndex(AlarmsTable.Columns.EVENT_ID));
 		intent.putExtra(BundleKeys.ALARM_DELETE_LECTURE_ID, lecture_id);
-		int day = cursor.getInt(7);
+		int day = cursor.getInt(cursor.getColumnIndex(AlarmsTable.Columns.DAY));
 		intent.putExtra(BundleKeys.ALARM_DELETE_DAY, day);
-		String title = cursor.getString(1);
+		String title = cursor.getString(cursor.getColumnIndex(AlarmsTable.Columns.EVENT_TITLE));
 		intent.putExtra(BundleKeys.ALARM_DELETE_TITLE, title);
-		long startTime = cursor.getLong(3);
+		long startTime = cursor.getLong(cursor.getColumnIndex(AlarmsTable.Columns.TIME));
 		intent.putExtra(BundleKeys.ALARM_DELETE_START_TIME, startTime);
 
 		intent.setAction("de.machtnix.fahrplan.ALARM");
@@ -113,7 +114,7 @@ public class AlarmList extends SherlockListActivity {
 		PendingIntent pendingintent = PendingIntent.getBroadcast(this, Integer.parseInt(lecture_id), intent, 0);
 		alarmManager.cancel(pendingintent);
 
-		String id = cursor.getString(0);
+		String id = cursor.getString(cursor.getColumnIndex(AlarmsTable.Columns.ID));
 		db.delete(AlarmsTable.NAME, Columns.ID + " = ?", new String[]{id});
 		cursor.requery();
 		mAdapter.notifyDataSetChanged();

src/nerd/tuxmobil/fahrplan/congress/FahrplanContract.java
@@ -1,5 +1,7 @@
 package nerd.tuxmobil.fahrplan.congress;
 
+import android.provider.BaseColumns;
+
 public interface FahrplanContract {
 
 	public interface FragmentTags {
@@ -12,13 +14,13 @@ public interface FahrplanContract {
 		public static final String NAME = "meta";
 
 		public interface Columns {
-			public static final String NUM_DAYS = "numdays";
-			public static final String VERSION = "version";
-			public static final String TITLE = "title";
-			public static final String SUBTITLE = "subtitle";
-			public static final String DAY_CHANGE_HOUR = "day_change_hour";
-			public static final String DAY_CHANGE_MINUTE = "day_change_minute";
-			public static final String ETAG = "etag";
+			public static final String NUM_DAYS = "numdays"; 						// 0
+			public static final String VERSION = "version"; 						// 1
+			public static final String TITLE = "title"; 							// 2
+			public static final String SUBTITLE = "subtitle"; 					// 3
+			public static final String DAY_CHANGE_HOUR = "day_change_hour"; 		// 4
+			public static final String DAY_CHANGE_MINUTE = "day_change_minute"; 	// 5
+			public static final String ETAG = "etag"; 							// 6
 		}
 
 		public interface Defaults {
@@ -35,14 +37,14 @@ public interface FahrplanContract {
 		public static final String NAME = "alarms";
 
 		public interface Columns {
-			public static final String ID = "_id";
-			public static final String EVENT_TITLE = "title";
-			public static final String ALARM_TIME_IN_MIN = "alarm_time_in_min";
-			public static final String TIME = "time";
-			public static final String TIME_TEXT = "timeText";
-			public static final String EVENT_ID = "eventid";
-			public static final String DISPLAY_TIME = "displayTime";
-			public static final String DAY = "day";
+			public static final String ID = "_id"; 								// 0
+			public static final String EVENT_TITLE = "title"; 					// 1
+			public static final String ALARM_TIME_IN_MIN = "alarm_time_in_min"; 	// 2
+			public static final String TIME = "time"; 							// 3
+			public static final String TIME_TEXT = "timeText"; 					// 4
+			public static final String EVENT_ID = "eventid"; 						// 5
+			public static final String DISPLAY_TIME = "displayTime"; 				// 6
+			public static final String DAY = "day"; 								// 7
 		}
 
 		public interface Defaults {
@@ -56,9 +58,9 @@ public interface FahrplanContract {
 		public static final String NAME = "highlight";
 
 		public interface Columns {
-			public static final String ID = "_id";
-			public static final String EVENT_ID = "eventid";
-			public static final String HIGHLIGHT = "highlight";
+			public static final String ID = "_id"; 				// 0
+			public static final String EVENT_ID = "eventid";		// 1
+			public static final String HIGHLIGHT = "highlight";	// 2
 		}
 
 		public interface Values {
@@ -72,27 +74,27 @@ public interface FahrplanContract {
 
 		public static final String NAME = "lectures";
 
-		public interface Columns {
-			public static final String EVENT_ID = "event_id";
-			public static final String TITLE = "title";
-			public static final String SUBTITLE = "subtitle";
-			public static final String DAY = "day";
-			public static final String ROOM = "room";
-			public static final String START = "start";
-			public static final String DURATION = "duration";
-			public static final String SPEAKERS = "speakers";
-			public static final String TRACK = "track";
-			public static final String TYPE = "type";
-			public static final String LANG = "lang";
-			public static final String ABSTRACT = "abstract";
-			public static final String DESCR = "descr";
-			public static final String REL_START = "relStart";
-			public static final String DATE = "date";
-			public static final String LINKS = "links";
-			public static final String DATE_UTC = "dateUTC";
-			public static final String ROOM_IDX = "room_idx";
-			public static final String REC_LICENSE = "rec_license";
-			public static final String REC_OPTOUT = "rec_optout";
+		public interface Columns extends BaseColumns {
+			public static final String EVENT_ID = "event_id"; 		// 00
+			public static final String TITLE = "title";				// 01
+			public static final String SUBTITLE = "subtitle";			// 02
+			public static final String DAY = "day";					// 03
+			public static final String ROOM = "room";					// 04
+			public static final String START = "start";				// 05
+			public static final String DURATION = "duration";			// 06
+			public static final String SPEAKERS = "speakers";			// 07
+			public static final String TRACK = "track";				// 08
+			public static final String TYPE = "type";					// 09
+			public static final String LANG = "lang";					// 10
+			public static final String ABSTRACT = "abstract";			// 11
+			public static final String DESCR = "descr";				// 12
+			public static final String REL_START = "relStart";		// 13
+			public static final String DATE = "date";					// 14
+			public static final String LINKS = "links";				// 15
+			public static final String DATE_UTC = "dateUTC";			// 16
+			public static final String ROOM_IDX = "room_idx";			// 17
+			public static final String REC_LICENSE = "rec_license";	// 18
+			public static final String REC_OPTOUT = "rec_optout";		// 19
 		}
 
 		public interface Defaults {

src/nerd/tuxmobil/fahrplan/congress/FahrplanMisc.java
@@ -57,8 +57,8 @@ public class FahrplanMisc {
 
 		cursor.moveToFirst();
 		while (!cursor.isAfterLast()) {
-			int day = cursor.getInt(3);
-			String date = cursor.getString(14);
+			int day = cursor.getInt(cursor.getColumnIndex(LecturesTable.Columns.DAY));
+			String date = cursor.getString(cursor.getColumnIndex(LecturesTable.Columns.DATE));
 
 			if (DateList.dateInList(MyApp.dateList, day) == false) {
 				MyApp.dateList.add(new DateList(day, date));
@@ -100,20 +100,27 @@ public class FahrplanMisc {
 
 		if (cursor.getCount() > 0) {
 			cursor.moveToFirst();
-			if (cursor.getColumnCount() > 0)
-				MyApp.numdays = cursor.getInt(0);
-			if (cursor.getColumnCount() > 1)
-				MyApp.version = cursor.getString(1);
-			if (cursor.getColumnCount() > 2)
-				MyApp.title = cursor.getString(2);
-			if (cursor.getColumnCount() > 3)
-				MyApp.subtitle = cursor.getString(3);
-			if (cursor.getColumnCount() > 4)
-				MyApp.dayChangeHour = cursor.getInt(4);
-			if (cursor.getColumnCount() > 5)
-				MyApp.dayChangeMinute = cursor.getInt(5);
-			if (cursor.getColumnCount() > 6)
-				MyApp.eTag = cursor.getString(6);
+			int columnIndexNumDays = cursor.getColumnIndex(MetasTable.Columns.NUM_DAYS);
+			if (cursor.getColumnCount() > columnIndexNumDays)
+				MyApp.numdays = cursor.getInt(columnIndexNumDays);
+			int columIndexVersion = cursor.getColumnIndex(MetasTable.Columns.VERSION);
+			if (cursor.getColumnCount() > columIndexVersion)
+				MyApp.version = cursor.getString(columIndexVersion);
+			int columnIndexTitle = cursor.getColumnIndex(MetasTable.Columns.TITLE);
+			if (cursor.getColumnCount() > columnIndexTitle)
+				MyApp.title = cursor.getString(columnIndexTitle);
+			int columnIndexSubTitle = cursor.getColumnIndex(MetasTable.Columns.SUBTITLE);
+			if (cursor.getColumnCount() > columnIndexSubTitle)
+				MyApp.subtitle = cursor.getString(columnIndexSubTitle);
+			int columnIndexDayChangeHour = cursor.getColumnIndex(MetasTable.Columns.DAY_CHANGE_HOUR);
+			if (cursor.getColumnCount() > columnIndexDayChangeHour)
+				MyApp.dayChangeHour = cursor.getInt(columnIndexDayChangeHour);
+			int columnIndexDayChangeMinute = cursor.getColumnIndex(MetasTable.Columns.DAY_CHANGE_MINUTE);
+			if (cursor.getColumnCount() > columnIndexDayChangeMinute)
+				MyApp.dayChangeMinute = cursor.getInt(columnIndexDayChangeMinute);
+			int columnIndexEtag = cursor.getColumnIndex(MetasTable.Columns.ETAG);
+			if (cursor.getColumnCount() > columnIndexEtag)
+				MyApp.eTag = cursor.getString(columnIndexEtag);
 		}
 
 		MyApp.LogDebug(LOG_TAG, "loadMeta: numdays=" + MyApp.numdays + " version:"
@@ -222,13 +229,13 @@ public class FahrplanMisc {
 
 		Intent intent = new Intent(context, AlarmReceiver.class);
 
-		String lecture_id = cursor.getString(5);
+		String lecture_id = cursor.getString(cursor.getColumnIndex(AlarmsTable.Columns.EVENT_ID));
 		intent.putExtra(BundleKeys.ALARM_DELETE_LECTURE_ID, lecture_id);
-		int day = cursor.getInt(7);
+		int day = cursor.getInt(cursor.getColumnIndex(AlarmsTable.Columns.DAY));
 		intent.putExtra(BundleKeys.ALARM_DELETE_DAY, day);
-		String title = cursor.getString(1);
+		String title = cursor.getString(cursor.getColumnIndex(AlarmsTable.Columns.EVENT_TITLE));
 		intent.putExtra(BundleKeys.ALARM_DELETE_TITLE, title);
-		long startTime = cursor.getLong(3);
+		long startTime = cursor.getLong(cursor.getColumnIndex(AlarmsTable.Columns.TIME));
 		intent.putExtra(BundleKeys.ALARM_DELETE_START_TIME, startTime);
 
 		// delete any previous alarms of this lecture

src/nerd/tuxmobil/fahrplan/congress/onBootReceiver.java
@@ -44,15 +44,15 @@ public final class onBootReceiver extends BroadcastReceiver {
 
 		cursor.moveToFirst();
 		while (!cursor.isAfterLast()) {
-			long alarmTime = cursor.getLong(2);
+			long alarmTime = cursor.getLong(cursor.getColumnIndex(AlarmsTable.Columns.ALARM_TIME_IN_MIN));
 			alarm.set(alarmTime);
 
 			if (now.before(alarm)) {
 				// set alarm
-				String lecture_id = cursor.getString(5);
-				int day = cursor.getInt(7);
-				String title = cursor.getString(1);
-				long startTime = cursor.getLong(3);
+				String lecture_id = cursor.getString(cursor.getColumnIndex(AlarmsTable.Columns.EVENT_ID));
+				int day = cursor.getInt(cursor.getColumnIndex(AlarmsTable.Columns.DAY));
+				String title = cursor.getString(cursor.getColumnIndex(AlarmsTable.Columns.EVENT_TITLE));
+				long startTime = cursor.getLong(cursor.getColumnIndex(AlarmsTable.Columns.TIME));
 
 				Intent alarmintent = new Intent(context, AlarmReceiver.class);
 				alarmintent.putExtra("lecture_id", lecture_id);

commit b643b1812c67acaf9f591dee4240a669a017c679

Bugfix invalid cursor reference.
+ Bug introduced in commit 92c52aa49bb8c5719f863f2a68bd93d2207e7844.

src/nerd/tuxmobil/fahrplan/congress/FahrplanFragment.java
@@ -737,8 +737,8 @@ public class FahrplanFragment extends SherlockFragment implements OnClickListene
 
 		hCursor.moveToFirst();
 		while (!hCursor.isAfterLast()) {
-			String lecture_id = hCursor.getString(cursor.getColumnIndex(HighlightsTable.Columns.EVENT_ID));
-			int highlightState = hCursor.getInt(cursor.getColumnIndex(HighlightsTable.Columns.HIGHLIGHT));
+			String lecture_id = hCursor.getString(hCursor.getColumnIndex(HighlightsTable.Columns.EVENT_ID));
+			int highlightState = hCursor.getInt(hCursor.getColumnIndex(HighlightsTable.Columns.HIGHLIGHT));
 			MyApp.LogDebug(LOG_TAG, "lecture "+lecture_id+" is hightlighted:" + highlightState);
 
 			for (Lecture lecture : MyApp.lectureList) {