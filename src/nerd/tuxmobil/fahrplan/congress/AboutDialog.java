0729f3985ca826beb71a7343ae826094ea1bd05d
Tobias Preuss <tobias.preuss@googlemail.com>
2014-09-03 19:45:21 +0200

    Remove copyright notice for 30C3 artwork for MRMCD 2014.

--- src/nerd/tuxmobil/fahrplan/congress/AboutDialog.java
+++ src/nerd/tuxmobil/fahrplan/congress/AboutDialog.java
@@ -46,10 +46,6 @@ public class AboutDialog extends SherlockDialogFragment {
 			text.setText("");
 		}
 
-		TextView logo_copyright = (TextView)view.findViewById(R.id.copyright_logo);
-		logo_copyright.setText(Html.fromHtml(getString(R.string.copyright_logo)));
-		logo_copyright.setMovementMethod(LinkMovementMethod.getInstance());
-
 		TextView conferenceUrl = (TextView)view.findViewById(R.id.conference_url);
 		conferenceUrl.setText(Html.fromHtml(getString(R.string.conference_url)));
 		conferenceUrl.setMovementMethod(LinkMovementMethod.getInstance());
