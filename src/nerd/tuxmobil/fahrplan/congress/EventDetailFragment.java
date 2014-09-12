c459db09595d5942d819a17fa5efb7f3ce50d4ab
Tobias Preuss <tobias.preuss@googlemail.com>
2014-09-12 20:26:04 +0200

    Change feedback URL for Datenspuren 2014 (not tested yet).

--- src/nerd/tuxmobil/fahrplan/congress/EventDetailFragment.java
+++ src/nerd/tuxmobil/fahrplan/congress/EventDetailFragment.java
@@ -37,7 +37,7 @@ public class EventDetailFragment extends SherlockFragment {
 	private final String LOG_TAG = "Detail";
 	private String event_id;
 	private String title;
-	private static String feedbackURL = "https://frab.cccv.de/en/30C3/public/events/%s/feedback/new";
+	private static String feedbackURL = "https://frab.cccv.de/en/DS2014/public/events/%s/feedback/new";
 	private Locale locale;
 	private Typeface boldCondensed;
 	private Typeface black;
