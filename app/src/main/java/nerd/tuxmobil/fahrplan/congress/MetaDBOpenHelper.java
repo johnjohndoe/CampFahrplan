e95a032adf0de7d16a56ca8989f2f693393400ae
Tobias Preuss <tobias.preuss@googlemail.com>
2015-10-21 15:40:57 +0200

    Clear database from Datenspuren 2014.

--- app/src/main/java/nerd/tuxmobil/fahrplan/congress/MetaDBOpenHelper.java
+++ app/src/main/java/nerd/tuxmobil/fahrplan/congress/MetaDBOpenHelper.java
@@ -10,7 +10,7 @@ import nerd.tuxmobil.fahrplan.congress.FahrplanContract.MetasTable.Defaults;
 
 public class MetaDBOpenHelper extends SQLiteOpenHelper {
 
-    private static final int DATABASE_VERSION = 3;
+    private static final int DATABASE_VERSION = 4;
 
     private static final String META_TABLE_CREATE =
             "CREATE TABLE " + MetasTable.NAME + " (" +
@@ -55,6 +55,11 @@ public class MetaDBOpenHelper extends SQLiteOpenHelper {
         if ((oldVersion < 3) && (newVersion >= 3)) {
             db.execSQL("ALTER TABLE " + MetasTable.NAME + " ADD COLUMN " +
                     Columns.ETAG + " TEXT DEFAULT " + Defaults.ETAG_DEFAULT);
+        }
+        if (oldVersion < 4) {
+            // Clear database from Datenspuren 2014
+            db.execSQL("DROP TABLE IF EXISTS " + MetasTable.NAME);
+            onCreate(db);
         }
     }
 }
