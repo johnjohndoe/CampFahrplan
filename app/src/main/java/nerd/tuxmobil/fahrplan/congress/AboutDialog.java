a0e359f79047de7a54e4f89fa0ea769077e20de8
Tobias Preuss <tobias.preuss@googlemail.com>
2016-08-12 15:08:31 +0200

    Add custom subtitle for FOSS4G 2016.

--- app/src/main/java/nerd/tuxmobil/fahrplan/congress/AboutDialog.java
+++ app/src/main/java/nerd/tuxmobil/fahrplan/congress/AboutDialog.java
@@ -33,7 +33,7 @@ public class AboutDialog extends DialogFragment {
         text = (TextView) view.findViewById(R.id.eventTitle);
         text.setText(MyApp.title);
         text = (TextView) view.findViewById(R.id.eventSubtitle);
-        text.setText(MyApp.subtitle);
+        text.setText("Welcome to Germany - Building Bridges");
         text = (TextView) view.findViewById(R.id.appVersion);
         String appVersionText = getString(R.string.appVersion, BuildConfig.VERSION_NAME);
         text.setText(appVersionText);
