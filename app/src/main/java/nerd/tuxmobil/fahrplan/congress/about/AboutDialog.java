246fadbc46a1fb1c5d483b8353671303cc2e1cb0
Tobias Preuss <tobias.preuss@googlemail.com>
2017-10-06 11:17:33 +0200

    Add custom subtitle for Hackover 2017.

--- app/src/main/java/nerd/tuxmobil/fahrplan/congress/about/AboutDialog.java
+++ app/src/main/java/nerd/tuxmobil/fahrplan/congress/about/AboutDialog.java
@@ -38,7 +38,7 @@ public class AboutDialog extends DialogFragment {
         text = (TextView) view.findViewById(R.id.eventTitle);
         text.setText(MyApp.title);
         text = (TextView) view.findViewById(R.id.eventSubtitle);
-        text.setText(MyApp.subtitle);
+        text.setText("6.-8. Oktober 2017, Nordstädter Bürgerschule, Hannover");
         text = (TextView) view.findViewById(R.id.appVersion);
         String appVersionText = getString(R.string.appVersion, BuildConfig.VERSION_NAME);
         text.setText(appVersionText);
