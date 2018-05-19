e754fee8701930ba6ad90162cb2d74db494c344e
Tobias Preuss <tobias.preuss@googlemail.com>
2018-05-19 15:14:46 +0200

    Clear database from PGCon 2017 on upgrade.

--- app/src/main/java/nerd/tuxmobil/fahrplan/congress/persistence/AlarmsDBOpenHelper.java
+++ app/src/main/java/nerd/tuxmobil/fahrplan/congress/persistence/AlarmsDBOpenHelper.java
@@ -8,7 +8,7 @@ import nerd.tuxmobil.fahrplan.congress.persistence.FahrplanContract.AlarmsTable;
 
 public class AlarmsDBOpenHelper extends SQLiteOpenHelper {
 
-    private static final int DATABASE_VERSION = 2;
+    private static final int DATABASE_VERSION = 4;
 
     private static final String DATABASE_NAME = "alarms";
 
@@ -39,6 +39,11 @@ public class AlarmsDBOpenHelper extends SQLiteOpenHelper {
             db.execSQL("ALTER TABLE " + AlarmsTable.NAME + " ADD " +
                     AlarmsTable.Columns.ALARM_TIME_IN_MIN + " INTEGER DEFAULT" +
                     AlarmsTable.Defaults.ALARM_TIME_IN_MIN_DEFAULT);
+        }
+        if (oldVersion < 4) {
+            // Clear alarm entries from PGCon 2017
+            db.execSQL("DROP TABLE IF EXISTS " + AlarmsTable.NAME);
+            onCreate(db);
         }
     }
 }
