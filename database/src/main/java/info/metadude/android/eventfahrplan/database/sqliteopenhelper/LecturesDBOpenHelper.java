76f7fc4c62468601d9d977b58ecf5cfc38702fb6
Tobias Preuss <tobias.preuss@googlemail.com>
2018-07-30 10:02:11 +0200

    Clear database from FrOSCon 2017 on upgrade.

--- database/src/main/java/info/metadude/android/eventfahrplan/database/sqliteopenhelper/LecturesDBOpenHelper.java
+++ database/src/main/java/info/metadude/android/eventfahrplan/database/sqliteopenhelper/LecturesDBOpenHelper.java
@@ -12,7 +12,7 @@ import info.metadude.android.eventfahrplan.database.contract.FahrplanContract.Le
 
 public class LecturesDBOpenHelper extends SQLiteOpenHelper {
 
-    private static final int DATABASE_VERSION = 6;
+    private static final int DATABASE_VERSION = 9;
 
     private static final String DATABASE_NAME = "lectures";
 
@@ -96,6 +96,11 @@ public class LecturesDBOpenHelper extends SQLiteOpenHelper {
         }
         if ((oldVersion < 6) && (newVersion >= 6)) {
             db.execSQL("ALTER TABLE " + LecturesTable.NAME + " ADD COLUMN " + Columns.SLUG + " TEXT DEFAULT ''");
+        }
+        if (oldVersion < 9) {
+            // Clear database from FrOSCon 2017
+            db.execSQL("DROP TABLE IF EXISTS " + LecturesTable.NAME);
+            onCreate(db);
         }
     }
 }
