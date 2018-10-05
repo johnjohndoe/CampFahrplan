a67e9ee3b1532105336e13bef25149ce9e17700c
Tobias Preuss <tobias.preuss@googlemail.com>
2018-10-05 12:32:46 +0200

    Hide schedule version for Hackover 2018.
    + The API does not provide the information.

--- app/src/main/java/nerd/tuxmobil/fahrplan/congress/about/AboutDialog.java
+++ app/src/main/java/nerd/tuxmobil/fahrplan/congress/about/AboutDialog.java
@@ -35,8 +35,14 @@ public class AboutDialog extends DialogFragment {
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        String scheduleVersion = MyApp.meta.getVersion();
         TextView text = view.findViewById(R.id.eventVersion);
-        text.setText(getString(R.string.fahrplan) + " " + MyApp.meta.getVersion());
+        if (TextUtils.isEmpty(scheduleVersion)) {
+            text.setVisibility(View.GONE);
+        } else {
+            text.setText(getString(R.string.fahrplan) + " " + scheduleVersion);
+            text.setVisibility(View.VISIBLE);
+        }
         text = view.findViewById(R.id.eventTitle);
         String title = MyApp.meta.getTitle();
         if (TextUtils.isEmpty(title)) {
