baf02adad6425a193fccb0567cede747754bda0e
Tobias Preuss <tobias.preuss@googlemail.com>
2022-01-08 18:06:15 +0100

    Reduce whitespace around "abstract" and "description" texts for FOSDEM 2022.

--- app/src/main/java/nerd/tuxmobil/fahrplan/congress/dataconverters/SessionExtensions.kt
+++ app/src/main/java/nerd/tuxmobil/fahrplan/congress/dataconverters/SessionExtensions.kt
@@ -169,6 +169,9 @@ fun Session.sanitize(): Session {
     if (abstractt == description) {
         abstractt = ""
     }
+    abstractt = abstractt.replace("<p>", "")
+    abstractt = abstractt.replace("</p>", "")
+    abstractt = abstractt.replace("\n", "<br>")
     if (createSpeakersString(speakers) == subtitle) {
         subtitle = ""
     }
@@ -176,6 +179,9 @@ fun Session.sanitize(): Session {
         description = abstractt
         abstractt = ""
     }
+    description = description.replace("<p>", "")
+    description = description.replace("</p>", "")
+    description = description.replace("\n", "<br>")
     if (!lang.isNullOrEmpty()) {
         lang = lang.lowercase()
     }
