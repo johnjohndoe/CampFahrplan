c2129b5ddf48edb23aacad1e3c53546cbfb37604
Tobias Preuss <tobias.preuss@googlemail.com>
2022-07-08 22:34:56 +0200

    Stop showing a session change on every response.
    + The speakers property of a session contains an invisible character.

--- network/src/main/java/info/metadude/android/eventfahrplan/network/serialization/XmlPullParsers.kt
+++ network/src/main/java/info/metadude/android/eventfahrplan/network/serialization/XmlPullParsers.kt
@@ -5,5 +5,7 @@ package info.metadude.android.eventfahrplan.network.serialization
 import androidx.annotation.NonNull
 import org.xmlpull.v1.XmlPullParser
 
+private const val ZERO_WIDTH_NO_BREAK_SPACE = '\uFEFF'
+
 @NonNull
-fun XmlPullParser.getSanitizedText(): String = text?.trim() ?: ""
+fun XmlPullParser.getSanitizedText(): String = text?.trim()?.trim(ZERO_WIDTH_NO_BREAK_SPACE) ?: ""
